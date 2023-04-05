d805f37d39d2cfab6bb6389bbde10667f3db42fe
Okoth Pius <orukopius8@gmail.com>
2023-04-05 13:14:34 +0300

    fix: update .htaccess file

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::resource('/', PostController::class)->names([
-    'index' => 'posts.index',
-    'create' => 'posts.create',
-    'store' => 'posts.store',
-    'show' => 'posts.show',
-]);
-
-Route::get('/posts/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::resource('posts', \App\Http\Controllers\PostController::class);
